18cc62d4541144bd3a70f096cc8da8069b5aacda
Dharavane-Ajit-au46 <101946480+Dharavane-Ajit-au46@users.noreply.github.com>
2023-01-17 23:04:02 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,9 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-// ==========================================
+
 //              AUTH ROUTES
-// ==========================================
-//REGISTER
+
 router.get("/register", function(req, res) {
     res.render("register");
 });
